db67c35df96f6de1b297dbfcc1401a6f6e741776
Diego Arca <155889956+diegoarca0124@users.noreply.github.com>
2024-12-29 22:50:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+});
